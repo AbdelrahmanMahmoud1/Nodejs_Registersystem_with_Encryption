3211be762a76ab0e67b11e1b256066b5adbe5617
Abdelrahman <abdelrahman.mabdelrasoul1@gmail.com>
2023-05-15 01:00:48 +0300

    Level3_Security_Hashing

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app = express();
 
-console.log(process.env.SECRET);
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -27,7 +27,6 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model('User', userSchema);
 
@@ -53,7 +52,7 @@ app.post("/register",function(req,res) {
     const newuser = new User({
 
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
 
@@ -73,7 +72,7 @@ app.post("/register",function(req,res) {
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
 
 
